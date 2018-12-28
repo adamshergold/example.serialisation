95256029411c25170befe08128cbaadd6f8663f9
Adam Shergold <mail@adamshergold.co.uk>
2018-12-28 09:55:41 +0000

    more tests

--- example.serialisation.binary.tests/test.binaryPeekStreamWrapper.fs
+++ example.serialisation.binary.tests/test.binaryPeekStreamWrapper.fs
@@ -7,6 +7,7 @@ open Xunit.Abstractions
 
 open Example.Serialisation
 open Example.Serialisation.Binary
+open Example.Serialisation.Binary
 open Example.Serialisation.TestTypes 
 
 type PeekStreamWrapperShould( oh: ITestOutputHelper ) = 
@@ -18,22 +19,116 @@ type PeekStreamWrapperShould( oh: ITestOutputHelper ) =
             
         Logging.CreateLogger options
 
-    let sut () =
+    let sut (s:System.IO.Stream) =
        
-        let ms = 
-            new System.IO.MemoryStream() 
-            
         let ss = 
-            SerdeStreamWrapper.Make( ms )
+            SerdeStreamWrapper.Make( s )
         
         BinaryPeekStreamWrapper.Make( ss ) 
+    
+    let testStream cb =
+        
+        let ms =
+            new System.IO.MemoryStream()
+            
+        use bw =
+            new System.IO.BinaryWriter( ms, System.Text.Encoding.Default, true )
+            
+        cb bw
+
+        bw.Close()
+        
+        ms.Position <- 0L
+
+        ms
         
     [<Fact>]
     member this.``BeCreateable`` () = 
     
-        let sut = sut()
-                    
+        use ms =
+            new System.IO.MemoryStream()
+            
+        let sut = sut( ms )
+        
         Assert.True( true )                    
 
+    
+    [<Fact>]
+    member this.``PeekAndReadString`` () = 
+    
+        let tv = "Hello!"
+       
+        use ts =
+            testStream ( fun bw -> bw.Write( tv ) )
         
+        let sut = sut( ts )
+
+        Assert.Equal( tv, sut.PeekString() )
+        Assert.Equal( tv, sut.ReadString() )
+
+        
+    [<Fact>]
+    member this.``ReadInt8`` () = 
+    
+        let tv = 6uy
+       
+        use ts =
+            testStream ( fun bw -> bw.Write( tv ) )
+        
+        let sut = sut( ts )
+
+        Assert.Equal( tv, sut.ReadInt8() )        
+
+        
+    [<Fact>]
+    member this.``ReadInt64`` () = 
+    
+        let tv = (int64) 123
+       
+        use ts =
+            testStream ( fun bw -> bw.Write( tv ) )
+        
+        let sut = sut( ts )
+
+        Assert.Equal( tv, sut.ReadInt64() )        
+        
+        
+    [<Fact>]
+    member this.``ReadDouble`` () = 
+    
+        let tv = 123.456
+        
+        use ts =
+            testStream ( fun bw -> bw.Write( tv ) )
+        
+        let sut = sut( ts )
+
+        Assert.Equal( tv, sut.ReadDouble() )        
+
+    [<Fact>]
+    member this.``ReadBool`` () = 
+    
+        let tv = false
+        
+        use ts =
+            testStream ( fun bw -> bw.Write( tv ) )
+        
+        let sut = sut( ts )
+
+        Assert.Equal( tv, sut.ReadBool() )        
+        
+    [<Fact>]
+    member this.``ReadBytes`` () = 
+    
+        let tv = Array.ofList [ 1uy; 2uy; 3uy ]
+        
+        use ts =
+            testStream ( fun bw -> bw.Write( tv ) )
+        
+        let sut = sut( ts )
+
+        let vs =
+            sut.ReadBytes( 3 )
+            
+        Assert.Equal( [ 1uy; 2uy; 3uy ], vs )      
         
